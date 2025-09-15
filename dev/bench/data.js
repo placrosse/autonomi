dcaa3163e1783cbdbe890484472fa707f6daabe3
github-action-benchmark <github@users.noreply.github.com>
2025-09-15 17:06:43 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 80608fae96e7d9465de352addb76eb5d0efd10ee

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757955928257,
+  "lastUpdate": 1757956003717,
   "repoUrl": "https://github.com/placrosse/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -39,6 +39,37 @@ window.BENCHMARK_DATA = {
             "name": "ant files download",
             "value": 22.140671571784086,
             "unit": "MiB/s"
+          }
+        ]
+      }
+    ],
+    "Node memory": [
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "80608fae96e7d9465de352addb76eb5d0efd10ee",
+          "message": "Merge pull request #3200 from grumbach/python_streaming\n\nfeat: python bindings for data stream",
+          "timestamp": "2025-09-11T16:42:21Z",
+          "tree_id": "2bad566299f577b285343daf624f27ffb207f035",
+          "url": "https://github.com/placrosse/autonomi/commit/80608fae96e7d9465de352addb76eb5d0efd10ee"
+        },
+        "date": 1757956003167,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 89,
+            "unit": "MB"
           }
         ]
       }
