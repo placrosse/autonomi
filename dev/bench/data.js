7e99d976d53f6c8becca80dd0b41449ae8f67d0e
github-action-benchmark <github@users.noreply.github.com>
2025-09-15 17:05:28 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for 80608fae96e7d9465de352addb76eb5d0efd10ee

---
+++ dev/bench/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1757955928257,
+  "repoUrl": "https://github.com/placrosse/autonomi",
+  "entries": {
+    "`safe files` benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "80608fae96e7d9465de352addb76eb5d0efd10ee",
+          "message": "Merge pull request #3200 from grumbach/python_streaming\n\nfeat: python bindings for data stream",
+          "timestamp": "2025-09-11T16:42:21Z",
+          "tree_id": "2bad566299f577b285343daf624f27ffb207f035",
+          "url": "https://github.com/placrosse/autonomi/commit/80608fae96e7d9465de352addb76eb5d0efd10ee"
+        },
+        "date": 1757955927363,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ant files upload 1mb",
+            "value": 2.201093213840966,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files upload 10mb",
+            "value": 9.383190467176508,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files download",
+            "value": 22.140671571784086,
+            "unit": "MiB/s"
+          }
+        ]
+      }
+    ]
+  }
+}
