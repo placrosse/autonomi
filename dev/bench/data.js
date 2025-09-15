7f1297a3ebf81a64325e762e4043d8d45a096f86
github-action-benchmark <github@users.noreply.github.com>
2025-09-15 17:06:46 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 80608fae96e7d9465de352addb76eb5d0efd10ee

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757956005228,
+  "lastUpdate": 1757956006731,
   "repoUrl": "https://github.com/placrosse/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -106,6 +106,47 @@ window.BENCHMARK_DATA = {
             "name": "Average memory usage w/ upload",
             "value": 15,
             "unit": "MB"
+          }
+        ]
+      }
+    ],
+    "swarm_driver long handlings": [
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "80608fae96e7d9465de352addb76eb5d0efd10ee",
+          "message": "Merge pull request #3200 from grumbach/python_streaming\n\nfeat: python bindings for data stream",
+          "timestamp": "2025-09-11T16:42:21Z",
+          "tree_id": "2bad566299f577b285343daf624f27ffb207f035",
+          "url": "https://github.com/placrosse/autonomi/commit/80608fae96e7d9465de352addb76eb5d0efd10ee"
+        },
+        "date": 1757956006203,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 10511,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 22260,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 2,
+            "unit": "ms"
           }
         ]
       }
