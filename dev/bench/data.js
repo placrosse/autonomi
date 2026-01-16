ae6d4f9bb6a14cec66d66505ea2159f09ce91bca
github-action-benchmark <github@users.noreply.github.com>
2026-01-16 23:36:18 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 72b9fb091046e3859960972dfd64e197c7324c92

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768606484307,
+  "lastUpdate": 1768606578227,
   "repoUrl": "https://github.com/placrosse/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -108,6 +108,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 89,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mickvd99@gmail.com",
+            "name": "Mick van Dijke",
+            "username": "mickvandijke"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "72b9fb091046e3859960972dfd64e197c7324c92",
+          "message": "Merge pull request #3394 from mickvandijke/feat-analytical-tools-get-quote\n\nfeat(ant): add `developer get-quote` command",
+          "timestamp": "2026-01-15T15:14:22Z",
+          "tree_id": "46e64ec2b8955568e55646a6a90d2a28bdfede97",
+          "url": "https://github.com/placrosse/autonomi/commit/72b9fb091046e3859960972dfd64e197c7324c92"
+        },
+        "date": 1768606577998,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 182,
             "unit": "MB"
           }
         ]
